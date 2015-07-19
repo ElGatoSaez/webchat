fb0ec21f2a4d85094da8dfe337b9d583ccdf0311
BuildTools <miguel2706@outlook.com>
2015-07-19 12:37:53 -0500

    To be continued

--- js/libirc.js
+++ js/libirc.js
@@ -1,10 +1,10 @@
 function Irc (host, port, nick) {
     this.host = host;
     this.port = port;
-    this.nick = nick;
-    this.realname = "Hira's WebChat"
-    this.user = "webchat"
     this.connection = new WebSocket('ws://'+this.host+':'+this.port);
+    this.connection.nick = nick;
+    this.connection.realname = "Hira's WebChat"
+    this.connection.user = "webchat"
     // When the connection is open, send some data to the server
     this.connection.onopen = function () {
       this.send('NICK ' + this.nick);
