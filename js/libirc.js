61a1475253d153a36160394651a05658d3961406
Ramiro Bou <bouramiro@gmail.com>
2015-07-19 15:58:17 -0300

    Adding basic IRC authentication.

--- js/libirc.js
+++ js/libirc.js
@@ -1,21 +1,55 @@
-function Irc (host, port, nick) {
+function Irc (host, port, nick, password) {
     this.host = host;
     this.port = port;
     this.connection = new WebSocket('ws://'+this.host+':'+this.port);
     this.connection.nick = nick;
-    this.connection.realname = "Hira's WebChat"
-    this.connection.user = "webchat"
+    this.connection.password = password;
+    this.connection.realname = "Hira's WebChat";
+    this.connection.user = "webchat";
+    
+    this.connected = false;
+    
     // When the connection is open, send some data to the server
     this.connection.onopen = function () {
-      this.send('NICK ' + this.nick);
-      this.send('USER ' + this.user + ' 8 * :' + this.realname);
+        this.lsend("CAP REQ :sasl");
+        //this.send('NICK ' + this.nick);
+        //this.send('USER ' + this.user + ' 8 * :' + this.realname);
     };
     // Log errors
     this.connection.onerror = function (error) {
       console.log('[ERROR] ' + error);
     };
+    
+    this.connection.lsend = function(data) { // log what we send
+        console.debug(data)
+        this.send(data);
+    }
+    
     // Log messages from the server
     this.connection.onmessage = function (e) {
-      console.log('[DEBUG] ' + e.data);
+        if(e.data == 'CONNECTED'){
+            console.info('Connected!');
+            this.connected = true
+            return;
+        }
+        ircdata = JSON.parse(e.data)
+        console.debug(e.data);
+        
+        // IRC parsing and stuff!
+        if(ircdata['verb'] == "CAP" && ircdata['params'][1] == "ACK"){ // SASL ACK!
+            this.lsend("AUTHENTICATE PLAIN");
+        }else if(ircdata['verb'] == "AUTHENTICATE" && ircdata['params'][0] == "+"){
+            this.lsend("AUTHENTICATE " + btoa(this.nick + "\0" + this.nick + "\0" + this.password));
+        }else if(ircdata['verb'] == "903"){ // SASL auth successful!
+            
+        }else if(ircdata['verb'] == "904"){ // SASL auth error
+            console.warn("Auth error.");
+            $('#nick').attr('disabled', false);
+            $('#password').attr('disabled', false);
+            $('#remember').attr('disabled', false);
+            $('#btnSubmit').button('');
+            $('#formAlertBox').html("<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button> <b>Error authenticating</b><br/> Maybe your user/password is wrong?");
+            $('#formAlertBox').removeClass('hidden');
+        }
     };
 }
--- js/scripts.js
+++ js/scripts.js
@@ -15,13 +15,23 @@ $.fn.serializeObject = function()
     return o;
 };
 
+function connect(data){
+    console.info("Connecting...");
+    $('#nick').attr('disabled', true);
+    $('#password').attr('disabled', true);
+    $('#remember').attr('disabled', true);
+    $('#btnSubmit').button('loading');
+    
+    var irc = new Irc("omega.hira.io", 8765, data["nick"], data["password"]);
+}
+
 $(document).ready(function(){
     $(function() {
-        $('#login_form').on('submit', function(e) {
-            e.preventDefault();  //prevent form from submitting
+        $('#btnSubmit').on('click', function() {
             var data = $('#login_form').serializeObject();
-	    console.log(data);
-            var irc = new Irc("omega.hira.io", 8765, data["nick"]);
+            console.debug(data);
+            
+            connect(data);
         });
     });
 });
