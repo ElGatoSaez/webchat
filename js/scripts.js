4d67efc8f3ad8f90e474d4ce0ac86b55d69fa3d1
Ramiro Bou <bouramiro@gmail.com>
2015-07-20 18:08:59 -0300

    COLORS!

--- js/scripts.js
+++ js/scripts.js
@@ -55,6 +55,9 @@ function changediv(){
             $('#joinGroup').modal('toggle');
         });
         $('#chattext').wysihtml5({toolbar: {"font-styles": false, "lists": false, "link": false, "image": false, "blockquote": false, "size":'xs'}});
+        
+        $('#chatStuffContainer .wysihtml5-toolbar .btn-group').append('<a class="btn btn-default dropdown-toggle btn-xs" data-toggle="dropdown" aria-expanded="false"><span class="glyphicon glyphicon-font"></span><span class="current-font">Black</span><b class="caret"></b></a><ul class="dropdown-menu"><li><a data-wysihtml5-command="foreColor" data-wysihtml5-command-value="grey" tabindex="-1" href="javascript:;" unselectable="on"><span class="wysiwyg-color-gray">Grey</span></a></li><li><a data-wysihtml5-command="foreColor" data-wysihtml5-command-value="black" tabindex="-1" href="javascript:;" unselectable="on">Black</a></li><li><a data-wysihtml5-command="foreColor" data-wysihtml5-command-value="blue" tabindex="-1" href="javascript:;" unselectable="on"><span class="wysiwyg-color-blue">Blue</span></a></li><li><a data-wysihtml5-command="foreColor" data-wysihtml5-command-value="green" tabindex="-1" href="javascript:;" unselectable="on"><span class="wysiwyg-color-gren">Green</span></a></li><li><a data-wysihtml5-command="foreColor" data-wysihtml5-command-value="red" tabindex="-1" href="javascript:;" unselectable="on"><span class="wysiwyg-color-red">Red</span></a></li><li><a data-wysihtml5-command="foreColor" data-wysihtml5-command-value="orange" tabindex="-1" href="javascript:;" unselectable="on"><span class="wysiwyg-color-orange">Orange</span></a></li><li><a data-wysihtml5-command="foreColor" data-wysihtml5-command-value="yellow" tabindex="-1" href="javascript:;" unselectable="on"><span class="wysiwyg-color-yellow">Yellow</span></a></li><li><a data-wysihtml5-command="foreColor" data-wysihtml5-command-value="cyan" tabindex="-1" href="javascript:;" unselectable="on"><span class="wysiwyg-color-cyan">Cyan</span></a></li><li><a data-wysihtml5-command="foreColor" data-wysihtml5-command-value="pink" tabindex="-1" href="javascript:;" unselectable="on"><span class="wysiwyg-color-pink">Pink</span></a></li></ul></li>')
+        
         $('#chattext').keydown(function (event) {
             if (event.keyCode == 13 && !event.shiftKey) {
                 sendMessage($('#chattext').data("wysihtml5").el.context.innerHTML);
@@ -78,17 +81,36 @@ function sendMessage(message){
     var clone = $('#group_list .active').clone() // TODO optimize
     clone.find('.badge').remove()
     $('#chattext').data("wysihtml5").el.context.innerHTML = "";
+        
     var parsed = '<div class="media well"><a href="#" class="pull-left"><img alt="{0}\'s avatar" src="http://lorempixel.com/64/64/" class="media-object"></a>\
                   <div class="media-body"><h4>{0}</h4>{1}</div></div>'.format(window.irc.connection.nick, message);
     var t = $('#message_box')[0].scrollHeight - $('#message_box').scrollTop();
     if(t==$('#message_box').outerHeight() || t==$('#message_box').outerHeight()-1 || t==$('#message_box').outerHeight()+1){var bottom=true}else{var bottom=false;}
     $("#frame-"+clone.html()).append(parsed);
     if(bottom){ $('#message_box').scrollTop($('#message_box').prop('scrollHeight'));}
-    message = message.replace(/<b>|<\/b>/g, '\002')
-    message = message.replace(/<i>|<\/i>/g, '\u001d')
-    message = message.replace(/<u>|<\/u>/g, '\u001f')
-    message = message.replace(/&nbsp;/g, ' ')
-    message = message.replace(/<p>|<\/p>/g, '')
+    
+    message = message.replace(/<span class="wysiwyg-color-white">/g, '\00300');
+    message = message.replace(/<span class="wysiwyg-color-black">/g, '\00301');
+    message = message.replace(/<span class="wysiwyg-color-navy">/g, '\00302');
+    message = message.replace(/<span class="wysiwyg-color-green">/g, '\00303');
+    message = message.replace(/<span class="wysiwyg-color-red">/g, '\00304');
+    message = message.replace(/<span class="wysiwyg-color-brown">/g, '\00305');
+    message = message.replace(/<span class="wysiwyg-color-purple">/g, '\00306');
+    message = message.replace(/<span class="wysiwyg-color-orange">/g, '\00307');
+    message = message.replace(/<span class="wysiwyg-color-yellow">/g, '\00308');
+    message = message.replace(/<span class="wysiwyg-color-lime">/g, '\00309');
+    message = message.replace(/<span class="wysiwyg-color-teal">/g, '\00310');
+    message = message.replace(/<span class="wysiwyg-color-cyan">/g, '\00311');
+    message = message.replace(/<span class="wysiwyg-color-blue">/g, '\00312');
+    message = message.replace(/<span class="wysiwyg-color-pink">/g, '\00313');
+    message = message.replace(/<span class="wysiwyg-color-gray">/g, '\00314');
+    message = message.replace(/<span class="wysiwyg-color-lightgray">/g, '\00315');
+    message = message.replace(/<\/span>/g, '\003');
+    message = message.replace(/<b>|<\/b>/g, '\002');
+    message = message.replace(/<i>|<\/i>/g, '\u001d');
+    message = message.replace(/<u>|<\/u>/g, '\u001f');
+    message = message.replace(/&nbsp;/g, ' ');
+    message = message.replace(/<p>|<\/p>/g, '');
     message = message.split('<br>')[0] // TODO: send the other messages
     window.irc.connection.lsend("PRIVMSG #" + clone.html() + ' :' + message);
     // TODO: Parse line breaks
