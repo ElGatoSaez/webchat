06ba191c3aac415263a8e97703a008bc9c4a9d99
BuildTools <miguel2706@outlook.com>
2015-07-20 12:17:37 -0500

    IRC colors in the chat!

--- js/scripts.js
+++ js/scripts.js
@@ -124,3 +124,30 @@ function escapeHtml(string) {
       return entityMap[s];
     });
 }
+
+function parseColors(message) {
+    var rex = /\003([0-9]{1,2})[,]?([0-9]{1,2})?([^\003]+)/,matches,colors;
+    if (rex.test(message)) {
+        while (cp = rex.exec(message)) {
+            if (cp[2]) {
+                var cbg = cp[2];
+            }
+            var message = message.replace(cp[0],'<span class="fg'+cp[1]+' bg'+cbg+'">'+cp[3]+'</span>');
+        }
+    }
+    var bui = [
+        [/\002([^\002]+)(\002)?/, ["<b>","</b>"]],
+        [/\037([^\037]+)(\037)?/, ["<u>","</u>"]],
+        [/\035([^\035]+)(\035)?/, ["<i>","</i>"]]
+    ];
+    for (var i=0;i < bui.length;i++) {
+        var bc = bui[i][0];
+        var style = bui[i][1];
+        if (bc.test(message)) {
+            while (bmatch = bc.exec(message)) {
+                var message = message.replace(bmatch[0], style[0]+bmatch[1]+style[1]);
+            }
+        }
+    }
+    return message;
+}
