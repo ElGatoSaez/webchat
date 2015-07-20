fa9f910768cf450460ffd49e931714bccc73113c
BuildTools <miguel2706@outlook.com>
2015-07-19 22:12:24 -0500

    Prevent xss!

--- js/scripts.js
+++ js/scripts.js
@@ -89,4 +89,17 @@ if (!String.prototype.format) {
     });
   };
 }
+ var entityMap = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': '&quot;',
+    "'": '&#39;',
+    "/": '&#x2F;'
+  };
 
+  function escapeHtml(string) {
+    return String(string).replace(/[&<>"'\/]/g, function (s) {
+      return entityMap[s];
+    });
+  }
